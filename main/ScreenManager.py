4b6c0db9f913052f66b31074af8514254220eebd
robertobs299 <robertobs299@gmail.com>
2024-06-09 23:23:42 +0200

    ya tenemos historial, pero sin poder volver

--- main/ScreenManager.py
+++ main/ScreenManager.py
@@ -216,7 +216,6 @@ def encrypt_password(password):
 
 class MyApp(MDApp):
     user = None
-    historial_list = []
     dark_theme = True
     current_id_quedada = 0
     confirm_dialog = None
@@ -260,6 +259,17 @@ class MyApp(MDApp):
         # Agregar cada quedada a la pantalla
         for quedada in quedadas:
             self.add_card(quedada)
+
+    def mostrar_historial(self):
+        # Limpiar la lista de quedadas existentes
+        self.root.get_screen('main').ids.historial_list.clear_widgets()
+
+        # Obtener las últimas cinco quedadas
+        historial = Quedada.get_quedadas_user_asist(self.user.id)
+
+        # Agregar cada quedada a la pantalla
+        for quedada in historial:
+            self.add_card_historial(quedada)
 
 
 #Metodo que crea las tarjetas de las quedadas y las añade al layout
@@ -340,6 +350,83 @@ class MyApp(MDApp):
         self.root.get_screen('main').ids.card_list.add_widget(card)
         self.root.get_screen('main').ids.card_list.height += card.height + dp(15)  # Update height of BoxLayout
 
+    def add_card_historial(self, quedada):
+        card_user = MDCard(
+            size_hint=(1, None),
+            height=dp(460),
+            pos_hint={"center_x": 0.5},
+            elevation=10,
+            radius=[15],
+        )
+
+        box_layout = MDBoxLayout(
+            orientation='vertical',
+            padding=dp(10),
+            spacing=dp(10),
+        )
+        if quedada.imagen is not None:
+            url = load_image_from_url(quedada.imagen)
+            image = Image(
+                source=url,  # Load image from URL
+                size_hint_y=None,
+                allow_stretch=True,
+                height=dp(250),
+            )
+            # Add the card to a layout or return it
+        else:
+            print("No hay imagen en la bbdd disponible para esta quedada")
+            image = Image(
+                source='moto.jpg',
+                size_hint_y=None,
+                allow_stretch=True,
+                height=dp(250),
+            )
+
+        title_label = MDLabel(
+            text=quedada.nombre,
+            halign='center',
+            size_hint_y=None,
+            height=dp(30),
+        )
+
+        description_layout = MDBoxLayout(
+            orientation='horizontal',
+            size_hint_y=None,
+            height=dp(60),
+        )
+
+        description_label = MDLabel(
+            text=quedada.descripcion,
+            size_hint_x=0.85,
+        )
+
+        sign_up_button = MDRaisedButton(
+            text='Ver Quedada',
+            md_bg_color=self.theme_cls.primary_color,
+            size_hint_x=0.15,
+            on_release=self.on_view_quedadas_button_press
+
+        )
+
+        description_layout.add_widget(description_label)
+        description_layout.add_widget(sign_up_button)
+
+        box_layout.add_widget(image)
+        box_layout.add_widget(title_label)
+        box_layout.add_widget(description_layout)
+
+        card_user.add_widget(box_layout)
+        card_user.data = {
+            "id": quedada.id_quedada,
+            "image": "default_image.jpg",
+            "title": quedada.nombre,
+            "participants": f'Nº de participantes: {quedada.numero_personas}',
+            "description": quedada.descripcion,
+        }
+
+        self.root.get_screen('main').ids.historial_list.add_widget(card_user)
+        self.root.get_screen('main').ids.historial_list.height += card_user.height + dp(15)  # Update height of BoxLayout
+
     def ver_id_quedada(self,instance):
         parent_card = instance.parent.parent.parent
         card_data = parent_card.data
@@ -366,66 +453,66 @@ class MyApp(MDApp):
     #     self.update_historial()
 
 #Metodo que actualiza el historial de quedadas a las que el usuario se ha apuntado
-    def update_historial(self):
-        historial_container = self.root.get_screen('main').ids.historial_list
-        historial_container.clear_widgets()
-
-        for event in self.historial_list:
-            card = MDCard(
-                size_hint=(None, None),
-                size=(dp(400), dp(460)),
-                pos_hint={"center_x": 0.5},
-                elevation=10,
-                radius=[15],
-            )
-
-            box_layout = MDBoxLayout(
-                orientation='vertical',
-                padding=dp(10),
-                spacing=dp(10),
-            )
-
-            image = Image(
-                source=event['image'],
-                size_hint_y=None,
-                allow_stretch=True,
-                height=dp(250),
-            )
-
-            title_label = MDLabel(
-                text=event['title'],
-                halign='center',
-                size_hint_y=None,
-                height=dp(30),
-            )
-
-            participants_label = MDLabel(
-                text=event['participants'],
-                size_hint_y=None,
-                height=dp(30),
-            )
-
-            description_layout = MDBoxLayout(
-                orientation='horizontal',
-                size_hint_y=None,
-                height=dp(60),
-            )
-
-            description_label = MDLabel(
-                text=event['description'],
-                size_hint_x=0.85,
-            )
-
-            description_layout.add_widget(description_label)
-
-            box_layout.add_widget(image)
-            box_layout.add_widget(title_label)
-            box_layout.add_widget(participants_label)
-            box_layout.add_widget(description_layout)
-
-            card.add_widget(box_layout)
-            historial_container.add_widget(card)
-            historial_container.height += card.height + dp(15)  # Update height of BoxLayout
+    # def update_historial(self):
+    #     historial_container = self.root.get_screen('main').ids.historial_list
+    #     historial_container.clear_widgets()
+    #
+    #     for event in self.historial_list:
+    #         card = MDCard(
+    #             size_hint=(None, None),
+    #             size=(dp(400), dp(460)),
+    #             pos_hint={"center_x": 0.5},
+    #             elevation=10,
+    #             radius=[15],
+    #         )
+    #
+    #         box_layout = MDBoxLayout(
+    #             orientation='vertical',
+    #             padding=dp(10),
+    #             spacing=dp(10),
+    #         )
+    #
+    #         image = Image(
+    #             source=event['image'],
+    #             size_hint_y=None,
+    #             allow_stretch=True,
+    #             height=dp(250),
+    #         )
+    #
+    #         title_label = MDLabel(
+    #             text=event['title'],
+    #             halign='center',
+    #             size_hint_y=None,
+    #             height=dp(30),
+    #         )
+    #
+    #         participants_label = MDLabel(
+    #             text=event['participants'],
+    #             size_hint_y=None,
+    #             height=dp(30),
+    #         )
+    #
+    #         description_layout = MDBoxLayout(
+    #             orientation='horizontal',
+    #             size_hint_y=None,
+    #             height=dp(60),
+    #         )
+    #
+    #         description_label = MDLabel(
+    #             text=event['description'],
+    #             size_hint_x=0.85,
+    #         )
+    #
+    #         description_layout.add_widget(description_label)
+    #
+    #         box_layout.add_widget(image)
+    #         box_layout.add_widget(title_label)
+    #         box_layout.add_widget(participants_label)
+    #         box_layout.add_widget(description_layout)
+    #
+    #         card.add_widget(box_layout)
+    #         historial_container.add_widget(card)
+    #         historial_container.height += card.height + dp(15)  # Update height of BoxLayout
 
 
 ######### CREAR QUEDADA ################################################################################################
@@ -624,6 +711,7 @@ class MyApp(MDApp):
                 print("Inicio de sesión correcto")
                 self.user = validated_user  # Guarda el usuario validado en self.user
                 self.root.current = 'main'
+                self.mostrar_historial()
             else:
                 self.root.get_screen('login').ids.error_username.opacity = 100
 
