447558a3f05f305b3b8e7b8ac4be2eb5c62ba3b5
robertobs299 <robertobs299@gmail.com>
2024-06-09 20:42:05 +0200

    arreglado ya se pueden subir imagenes en las dos y el accrousel vuelve y se actualizan las quedadas al crear una nueva

--- main/ScreenManager.py
+++ main/ScreenManager.py
@@ -251,7 +251,13 @@ class MyApp(MDApp):
 
         self.dark_theme = not self.dark_theme  # Cambia el estado del tema
     def on_start(self):
+        # Limpiar la lista de quedadas existentes
+        self.root.get_screen('main').ids.card_list.clear_widgets()
+
+        # Obtener las últimas cinco quedadas
         quedadas = Quedada.get_last_five()
+
+        # Agregar cada quedada a la pantalla
         for quedada in quedadas:
             self.add_card(quedada)
 
@@ -540,25 +546,39 @@ class MyApp(MDApp):
     def crear_quedada(self):
         id_cp = obtener_id_cp(self.root.get_screen('main').ids.cp.text)
 
-        id_direccion = insertar_direccion(self.root.get_screen('main').ids.tipo_via.text, self.root.get_screen('main').ids.direccion.text, id_cp,
+        id_direccion = insertar_direccion(self.root.get_screen('main').ids.tipo_via.text,
+                                          self.root.get_screen('main').ids.direccion.text, id_cp,
                                           self.root.get_screen('main').ids.numero.text)
 
-        id_corrdenadas = insertarCoordenadas(self.root.get_screen('main').ids.map_view.lat, self.root.get_screen('main').ids.map_view.lon)
+        id_corrdenadas = insertarCoordenadas(self.root.get_screen('main').ids.map_view.lat,
+                                             self.root.get_screen('main').ids.map_view.lon)
 
         actualizar_coordenadas_direccion(id_direccion, id_corrdenadas)
 
-        quedada = Quedada(None, self.root.get_screen('main').ids.nombre.text, self.root.get_screen('main').ids.descripcion.text, self.user.id, self.fecha, self.hora,
+        quedada = Quedada(None, self.root.get_screen('main').ids.nombre.text,
+                          self.root.get_screen('main').ids.descripcion.text, self.user.id, self.fecha, self.hora,
                           id_direccion, self.root.get_screen('main').ids.max_personas.text, 0, 1, self.ruta_imagen)
 
         idQuedada = quedada.insertar_quedada()
         self.reset_and_go_to_first_screen()
         if self.ruta_imagen != None:
-            Quedada.updateFotoQuedada(idQuedada,self.ruta_imagen)
+            Quedada.updateFotoQuedada(idQuedada, self.ruta_imagen)
+
+        # Actualizar la pantalla de quedadas
+        self.on_start()
 
     def file_manager_open(self):
         self.file_manager.show('/')
 
     def select_path(self, path):
+        self.exit_manager()
+        # ver en que pantalla esta y si esta en main llamar a select_path_crear_quedada y si esta en info_quedada llamar a select_path_info_quedada
+        if self.root.current == 'main':
+            self.select_path_crear_quedada(path)
+        if self.root.current == 'info_quedada':
+            self.select_path_info_quedada(path)
+
+    def select_path_crear_quedada(self, path):
         self.exit_manager()
         self.upload_image_to_server_and_save_to_db(path)
 
@@ -779,18 +799,15 @@ class MyApp(MDApp):
 
 #Actualiza la galeria de fotos para que se vayan mostrando
     def change_slide(self, dt):
-        if len(self.root.get_screen('ver_quedada').ids.carousel.slides) > 1:
-            self.root.get_screen('ver_quedada').ids.carousel.load_next()
-#Metodo que permite abrir el gestor de archivos
-    def file_manager_open(self):
-        self.file_manager.show('/')
+        carrousel = self.root.get_screen('ver_quedada').ids.carousel
+        if carrousel.index == len(carrousel.slides) - 1:
+            carrousel.index = 0
+        else:
+            carrousel.load_next()
 #Metodo que permite almacenar la ruta del archivo
-    def select_path(self, path):
+    def select_path_info_quedada(self, path):
         self.exit_manager()
         self.add_image_to_carousel_and_db(path)
-#Metodo que cierra el gestor de archivos
-    def exit_manager(self, *args):
-        self.file_manager.close()
 #Metodo que añade una imagen al carrusel y la sube al servidor
     def add_image_to_carousel_and_db(self, path):
         image = AsyncImage(source=path)
